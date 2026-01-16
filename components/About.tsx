cf408486e6980d6ebf888631f53b9ee767d6374c
ferm <fermandrei1@gmail.com>
2026-01-16 19:27:10 +0800

    idunno

--- components/About.tsx
+++ components/About.tsx
@@ -64,54 +64,51 @@ const About = ({ careerStats, aboutMeDesc }: AboutProps) => {
           ariaLabel="Technology partners"
         />
       </div> */}
-      <div className="grid grid-cols-2 gap-2 mx-auto max-w-7xl mt-10 rounded-lg bg-neutral-800/30 backdrop-blur-md backdrop-saturate-150 p-5">
-        <h1 className="col-span-2 mx-auto text-3xl font-extrabold mb-4 text-white">
-          ABOUT ME
-        </h1>
-        <p className="whitespace-pre-line leading-relaxed text-white">
-          {aboutMeDesc}
-        </p>
-        {/* <div className="relative ">
+      <div className="gap-2 mx-auto max-w-7xl mt-10 ">
+        <div className="mx-4 rounded-lg bg-neutral-800/30 backdrop-blur-md backdrop-saturate-150 p-5">
+          <h1 className="text-center col-span-2 mx-auto text-3xl font-extrabold mb-4 text-white">
+            ABOUT ME
+          </h1>
+          <p className="whitespace-pre-line leading-relaxed text-white text-lg">
+            {aboutMeDesc}
+          </p>
+          {/* <div className="relative ">
           <Image src="/logo.png" alt="Logo" fill className="object-contain" />
         </div> */}
-        <div className="grid grid-cols-2 gap-4 p-10">
-          {careerStats.map((stat) => (
-            <div
-              key={stat.id}
-              className="w-60 rounded-lg px-6 py-2 font-medium
-                        text-white dark:text-black
-                        bg-neutral-800/30 dark:bg-white/70
-                        backdrop-blur-md backdrop-saturate-150
-                        border border-white/5
-                        shadow-[0_8px_32px_rgba(0,0,0,0.12)]
-                        transition-all duration-300
-                        hover:-translate-y-0.5
-                        hover:shadow-[0_12px_40px_rgba(0,0,0,0.18)]
-                        dark:shadow-[0_8px_32px_rgba(0,0,0,0.08)]"
-            >
-              <div className="flex items-center">
-                {/* <Image
+          <div className="grid gap-4 p-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {careerStats.map((stat) => (
+              <div
+                key={stat.id}
+                className="flex flex-col items-start rounded-lg px-6 py-4 font-medium
+             text-white dark:text-black
+             bg-neutral-800/30 dark:bg-white/70
+             backdrop-blur-md backdrop-saturate-150
+             border border-white/5"
+              >
+                <div className="flex items-center">
+                  {/* <Image
                   src={stat.icon}
                   alt={stat.label}
                   width={35}
                   height={35}
                   className="mr-2"
                 /> */}
-                <CountUp
-                  from={0}
-                  to={stat.value}
-                  separator=","
-                  direction="up"
-                  duration={1}
-                  className="count-up-text text-2xl font-bold"
-                />
-                {stat.label === "Technologies" && (
-                  <span className="text-lg font-bold">+</span>
-                )}
+                  <CountUp
+                    from={0}
+                    to={stat.value}
+                    separator=","
+                    direction="up"
+                    duration={1}
+                    className="count-up-text text-2xl font-bold"
+                  />
+                  {stat.label === "Technologies" && (
+                    <span className="text-lg font-bold">+</span>
+                  )}
+                </div>
+                <p className="text-lg font-semibold">{stat.label}</p>
               </div>
-              <p className="text-lg font-semibold">{stat.label}</p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </>
